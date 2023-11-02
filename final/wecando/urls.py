27e7375695b558e648c0d1c3ae52148a16743e19
DESKTOP-LUCTNC5\user <dhgudehd979@gmail.com>
2023-11-02 17:47:14 +0900

    2023-11-02 마무리 대문페이지 완료 일기장 페이지 디자인 완료 로그인 기능 구현 완료

--- final/wecando/urls.py
+++ final/wecando/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("diary_create/", views.DiaryCreate.as_view(), name="diary_create"),
     path("diary_write/", views.DiaryWrite.as_view(), name="diary_write"),
+    path('accounts/', include('allauth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='wecando/landing.html'), name='login'), # django.contrib.auth의 views를 import해서 따로 views에 입력하지 않고 기능 구현
 ]
