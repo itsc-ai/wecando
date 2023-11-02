5d927aa56b289a1428a26ea43bf20c038d4ca195
hdus13 <hdus13@naver.com>
2023-11-02 17:49:00 +0900

    calendar.html에 다운받은 캘린더 양식 삽입 일기 작성 폼 생성

--- final/final/settings.py
+++ final/final/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "wecando"
-    "crispy_forms",
-    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -124,5 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
