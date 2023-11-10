0b77e96bd9368e9b14e2c264a46edeb4e429755a
hdus13 <hdus13@naver.com>
2023-11-10 09:11:05 +0900

    일기 수정할 forms.py models.py 수정 일기 수정, 삭제 views.py 수정 일기 수정할 페이지 생성

--- final/wecando/admin.py
+++ final/wecando/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-from .models import Writen
+from .models import Writen, Diary
 admin.site.register(Writen)
+admin.site.register(Diary)
--- final/wecando/urls.py
+++ final/wecando/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.MainPage.as_view(), name="main_page"),
     path("diary/", views.DiaryMain.as_view(), name="diary"),
     path("calendar/<int:q>/", views.Calendar.as_view(), name="calendar"),
-    path("diary_detail/<int:pk>/", views.DiaryDetail.as_view(), name="diary_detail"),
+    path("diary_detail/<int:pk>/", views.diary_detail, name="diary_detail"),
     path("signup/", views.signup, name="signup"),
     path("diary_create/", views.DiaryCreate.as_view(), name="diary_create"),
     path("diary_write/<int:q>/", views.DiaryWrite.as_view(), name="diary_write"),
     path('accounts/', include('allauth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='wecando/landing.html'), name='login'), # django.contrib.auth의 views를 import해서 따로 views에 입력하지 않고 기능 구현
-]
+    path("write_delete/<int:pk>/", views.write_delete, name='write_delete'),
+    path("diary_update/<int:pk>/", views.DiaryUpdate.as_view(), name="diary_update"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
